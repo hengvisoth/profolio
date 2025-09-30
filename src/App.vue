b03ee00f7985cc258928d5ce4d4c11a11887d308
hengvisoth <hengratanakvisoth20@kit.edu.kh>
2025-09-30 20:06:43 +0700

    Refactor background

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
+  <DynamicBackdrop />
   <NavigationBar />
   <RouterView />
 </template>
 
 <script setup lang="ts">
 import NavigationBar from "../src/components/NavigationBar.vue";
+import DynamicBackdrop from "./components/visuals/DynamicBackdrop.vue";
 import { onMounted } from "vue";
 import { RouterLink, RouterView } from "vue-router";
 import { initFlowbite } from "flowbite";
